18d4e54b69c41b2d10a575037c0c5f146f1af93d
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2022-04-16 00:47:13 +0200

    Fix redirection. Nested redirection remains

--- flush.c
+++ flush.c
@@ -45,8 +45,54 @@ void prompt_user(char * cwd, char * input)
     }
 }
 
+void redirection(char *args[], char *cwd){
+    int fd;
+    int i = 0;
+    int redirect = 0;
+    char absolute_path_to_file[MAX_SIZE];
+    memset(absolute_path_to_file, '\0', sizeof(absolute_path_to_file));
+    while (args[i] != NULL){
+        if (strcmp(args[i],">") == 0 || strcmp(args[i],"<") == 0){
+            strcat(absolute_path_to_file, cwd);
+            strcat(absolute_path_to_file, "/");
+            strcat(absolute_path_to_file, args[i+1]);
+            redirect = 1;
+            if (strcmp(args[i], ">") == 0){
+                fd = open(absolute_path_to_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+                if(fd < 0){
+                    perror("Noe gikk galt kan ikke aapne fil");
+                }
+                printf("%d\n", fd);
+                dup2(fd, 1);
+                close(fd);
+                break;
+            }
+            if (strcmp(args[i], "<") == 0){
+                // Tatt fra stackoverflow
+                fd = open(absolute_path_to_file, O_RDONLY);
+                if (fd < 0){
+                    perror("Cannot open file");
+                    exit(0);
+                }
+                dup2(fd, 0);
+                close(fd);
+                break;
+            }
+        }
+        i++;
+    }
+    //Slicer slik at man faar inn riktig argumenter i execvp
+    if (redirect){
+        for (int j = i; j < MAX_STRING_LEN; j++){
+            args[j] = '\0';
+        }
+    }
+}
+
+
 void split_string(char *str, char *args[], char *cwd)
 {
+
     int counter = 1;
     char delim[] = " \t";
     char *ptr = strtok(str, delim);
@@ -54,42 +100,7 @@ void split_string(char *str, char *args[], char *cwd)
     while (ptr != NULL)
     {  
         ptr = strtok(NULL, delim);
-        if (ptr !=NULL){
-            if (strcmp(ptr, ">") == 0){
-                int fd;
-                char absolute_path_to_file[MAX_SIZE];
-                memset(absolute_path_to_file, '\0', sizeof(absolute_path_to_file));
-                // printf("cwd: %s", cwd);
-                strcat(absolute_path_to_file, cwd);
-                ptr = strtok(NULL, delim);
-                strcat(absolute_path_to_file, "/");
-                strcat(absolute_path_to_file, ptr);
-                // Tatt fra stackoverflow
-                if(fd = open(absolute_path_to_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)){
-                    perror("Noe gikk galt kan ikke aapne fil");
-                }
-                close(0);
-                dup2(fd, 1);
-                break;
-            }
-            if (strcmp(ptr, "<") == 0){
-                int fd0;
-                char absolute_path_to_file[MAX_SIZE];
-                memset(absolute_path_to_file, '\0', sizeof(absolute_path_to_file));
-                strcat(absolute_path_to_file, cwd);
-                ptr = strtok(NULL, delim);
-                strcat(absolute_path_to_file,  "/");
-                strcat(absolute_path_to_file, ptr);
-                // Tatt fra stackoverflow
-                if (fd0 = open(absolute_path_to_file, O_RDONLY) < 0){
-                    perror("Cannot open file");
-                    exit(0);
-                }
-                dup2(fd0, 0);
-                close(0);
-                return;
-            }
-        }
+        
         args[counter] = ptr;
         counter++;
     }
@@ -102,7 +113,6 @@ void split_string(char *str, char *args[], char *cwd)
     // args[counter] = NULL;
 }
 
-// void redirect_to_file()
 
 int flush()
 {
@@ -111,10 +121,6 @@ int flush()
     char* input_pointer_array[3];
     while(1)
     {
-        // set_cwd(cwd);
-        // prompt_user(cwd, input);
-        // char *args[4];
-        // split_string(input, args);
         set_cwd(cwd);
         prompt_user(cwd, input);
         char *args[MAX_STRING_LEN];
@@ -124,66 +130,7 @@ int flush()
         
         // child
         if (pid == 0){
-            // int counter = 1;
-            // char delim[] = " \t";
-            // char *ptr = strtok(input, delim);
-            // args[0] = ptr;
-            // while (ptr != NULL)
-            // {
-                
-            //     ptr = strtok(NULL, delim);
-            //     // ptr == ">"
-            //     // ptr == "<"
-
-            //     // Kjør feks ls > text.txt og ls vil bli sendt inn i text.txt
-            //     if (strcmp(ptr, ">") == 0){
-            //         int fd;
-            //         printf("fdsafdsa\n");
-            //         char full_path_to_file[MAX_SIZE];
-            //         strcat(full_path_to_file, cwd);
-            //         ptr = strtok(NULL, delim);
-            //         strcat(full_path_to_file, "/");
-            //         strcat(full_path_to_file, ptr);
-            //         printf("%s\n", full_path_to_file);
-            //         // Tatt fra stackoverflow
-            //         fd = open(full_path_to_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-            //         close(1);
-            //         dup2(fd, 1);
-            //         // execvp(args[0], args);
-            //         break;
-            //     }
-            //     if (strcmp(ptr, "<") == 0){
-            //         int fd0;
-            //         char full_path_to_file[MAX_SIZE];
-            //         strcat(full_path_to_file, cwd);
-            //         ptr = strtok(NULL, delim);
-            //         strcat(full_path_to_file, "/");
-            //         strcat(full_path_to_file, ptr);
-            //         printf("%s fdsafdsa\n", full_path_to_file);
-            //         // Tatt fra stackoverflow
-            //         if (fd0 = open(full_path_to_file, O_RDONLY) < 0){
-            //             perror("Cannot open file");
-            //             exit(0);
-            //         }
-            //         printf("%d\n", fd0);
-            //         close(0);
-            //         dup2(fd0, 0);
-            //         break;
-            //     }
-            //     if (strcmp(args[0], "cd") == 0)
-            //     {
-            //         chdir(args[1]);
-            //     }
-
-            //     args[counter] = ptr;
-            //     counter++;
-
-            // }
-            // for (int i = 0; i < MAX_STRING_LEN; i++){
-            //         printf("%s\n", args[i]);
-            //     }
-            // printf("haiehihfe\n");
-            printf("hei printes\n");
+            redirection(args, cwd);
             execvp(args[0], args);
             exit(0);
         }
