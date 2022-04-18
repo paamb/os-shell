22a43a1d37f09cd29b7047349f49b4000424ef99
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2022-04-18 18:29:21 +0200

    add option for wait backgroundprocess

--- flush.c
+++ flush.c
@@ -10,15 +10,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define MAX_SIZE 300
+#define MAX_STRING_LEN 10
 
-# define MAX_SIZE 300
-# define MAX_STRING_LEN 10
-
-struct Background_p {
+struct Background_p
+{
     int pid;
-    struct Background_p* next;    
+    struct Background_p *next;
 };
-void set_cwd(char* cwd)
+void set_cwd(char *cwd)
 {
     char cwd_array[200];
 
@@ -33,42 +33,50 @@ void set_cwd(char* cwd)
     }
 }
 
-void prompt_user(char * cwd, char * input)
+void prompt_user(char *cwd, char *input)
 {
     // bzero((char *)input, MAX_SIZE);
     printf("%s: ", cwd);
-    
-    if (fgets(input, MAX_SIZE, stdin) == NULL){
+
+    if (fgets(input, MAX_SIZE, stdin) == NULL)
+    {
         printf("\n");
         exit(0);
     }
-    //Remove newline character from input
+    // Remove newline character from input
 
-    if (input[0] != '\n'){
+    if (input[0] != '\n')
+    {
         input[strcspn(input, "\n")] = 0;
     }
 }
 
-void redirection(char *args[], char *cwd){
+void redirection(char *args[], char *cwd)
+{
     int fd;
     int i = 0;
     int redirect = 0;
     int command_ends = 0;
     char absolute_path_to_file[MAX_SIZE];
-    
-    while (args[i] != NULL){
+
+    while (args[i] != NULL)
+    {
         memset(absolute_path_to_file, '\0', sizeof(absolute_path_to_file));
-        if (strcmp(args[i],">") == 0 || strcmp(args[i],"<") == 0){
+        if (strcmp(args[i], ">") == 0 || strcmp(args[i], "<") == 0)
+        {
             strcat(absolute_path_to_file, cwd);
             strcat(absolute_path_to_file, "/");
-            strcat(absolute_path_to_file, args[i+1]);
+            strcat(absolute_path_to_file, args[i + 1]);
             redirect = 1;
-            if (command_ends == 0){
+            if (command_ends == 0)
+            {
                 command_ends = i;
-            } 
-            if (strcmp(args[i], ">") == 0){
+            }
+            if (strcmp(args[i], ">") == 0)
+            {
                 fd = open(absolute_path_to_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-                if(fd < 0){
+                if (fd < 0)
+                {
                     perror("Noe gikk galt kan ikke aapne fil");
                 }
                 printf("%d\n", fd);
@@ -77,10 +85,12 @@ void redirection(char *args[], char *cwd){
                 dup2(fd, 1);
                 close(fd);
             }
-            if (strcmp(args[i], "<") == 0){
+            if (strcmp(args[i], "<") == 0)
+            {
                 // Tatt fra stackoverflow
                 fd = open(absolute_path_to_file, O_RDONLY);
-                if (fd < 0){
+                if (fd < 0)
+                {
                     perror("Cannot open file");
                     exit(0);
                 }
@@ -90,20 +100,29 @@ void redirection(char *args[], char *cwd){
         }
         i++;
     }
-    //Slicer slik at man faar inn riktig argumenter i execvp
-    if (redirect){
-        for (int j = command_ends; j < MAX_STRING_LEN; j++){
+    // Slicer slik at man faar inn riktig argumenter i execvp
+    if (redirect)
+    {
+        for (int j = command_ends; j < MAX_STRING_LEN; j++)
+        {
             args[j] = '\0';
         }
     }
 }
 
-int is_background_process(char *args[]){
-    for (int i = 0; i < MAX_STRING_LEN; i++){
-        if (strcmp(args[i], "&") == 0 && args[i+1] == NULL){
+int is_background_process(char *args[])
+{
+    for (int i = 0; i < MAX_STRING_LEN; i++)
+    {
+        if (args[i] == NULL)
+        {
+            return 0;
+        }
+        if (strcmp(args[i], "&") == 0 && args[i + 1] == NULL)
+        {
             args[i] = '\0';
             return 1;
-        } 
+        }
     }
     return 0;
 }
@@ -116,9 +135,9 @@ void split_string(char *str, char *args[], char *cwd)
     char *ptr = strtok(str, delim);
     args[0] = ptr;
     while (ptr != NULL)
-    {  
+    {
         ptr = strtok(NULL, delim);
-        
+
         args[counter] = ptr;
         counter++;
     }
@@ -126,30 +145,30 @@ void split_string(char *str, char *args[], char *cwd)
     {
         chdir(args[1]);
     };
-    
+
     // for (int i = 0; i < MAX_STRING_LEN; i++){
     //     printf("Lines: %s\n", args[i]);
     //     if (args[i] == 0){
     //         break;
     //     }
-    //     // if (strcmp(args[i], "&") == 0 && strcmp(args[i+1]) == '\0') 
+    //     // if (strcmp(args[i], "&") == 0 && strcmp(args[i+1]) == '\0')
     // }
     // Maa ende paa null men ser ut til at det skjer uansett
     // args[counter] = NULL;
 }
 
-
 int flush()
 {
-    char* cwd = malloc(MAX_SIZE);
-    char* input = malloc(MAX_SIZE);
-    char* input_pointer_array[3];
+    char *cwd = malloc(MAX_SIZE);
+    char *input = malloc(MAX_SIZE);
+    char *input_pointer_array[3];
     int background_process = 0;
+    int wait_option = 0;
 
     // Initializing linked list
-    struct Background_p* head = NULL;
-    
-    while(1)
+    struct Background_p *head = NULL;
+
+    while (1)
     {
         background_process = 0;
         set_cwd(cwd);
@@ -157,14 +176,16 @@ int flush()
         char *args[MAX_STRING_LEN];
         split_string(input, args, cwd);
         background_process = is_background_process(args);
+        printf("hei\n");
         printf("%d\n", background_process);
         int pid = fork();
-        
-        
+
         // child
-        if (pid == 0){
-            if (background_process){
-                
+        if (pid == 0)
+        {
+            if (background_process)
+            {
+                wait_option = WNOHANG;
             }
             redirection(args, cwd);
             execvp(args[0], args);
@@ -173,11 +194,13 @@ int flush()
         }
 
         // parent waiting for child
-        else{
+        else
+        {
             int status;
-            waitpid(pid, &status, 0);
+            waitpid(pid, &status, wait_option);
             printf("Parent: %d \n", pid);
-            if(WIFEXITED(status)){
+            if (WIFEXITED(status))
+            {
                 int exit_status = WEXITSTATUS(status);
                 printf("Exit status = %d \n", exit_status);
             }
